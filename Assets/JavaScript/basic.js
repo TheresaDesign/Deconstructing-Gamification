ffb10109d2d46da6907b51710192645825753253
TheresaDesign <resab@t-online.de>
2024-12-21 19:53:17 +0100

    schriften mit Google verlinkt+ Quellen grob gestaltet

---
+++ Assets/JavaScript/basic.js
@@ -0,0 +1,9 @@
+function togglePopup() {
+    const popup = document.getElementById('popup');
+    if (popup.style.display === 'none' || popup.style.display === '') {
+      popup.style.display = 'block';
+    } else {
+      popup.style.display = 'none';
+    }
+  }
+  
