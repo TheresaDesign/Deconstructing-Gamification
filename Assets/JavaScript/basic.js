78fad0ea41c1da66976b672c6b11a213e7e42ef3
TheresaDesign <resab@t-online.de>
2024-12-22 15:30:15 +0100

    Einleitung positioniert

--- Assets/JavaScript/basic.js
+++ Assets/JavaScript/basic.js
@@ -7,3 +7,24 @@ function togglePopup() {
     }
   }
   
+//menu
+
+  const chapters = document.querySelectorAll(".chapter-point");
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const index = Array.from(sections).indexOf(entry.target);
+      if (entry.isIntersecting) {
+        chapters.forEach((chapter) => chapter.classList.remove("active"));
+        if (chapters[index]) {
+          chapters[index].classList.add("active");
+        }
+      }
+    });
+  },
+  { threshold: 0.5 } // Aktiviert, wenn 50% der Section im Viewport sind
+);
+
+sections.forEach((section) => observer.observe(section));
