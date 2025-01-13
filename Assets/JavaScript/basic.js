a2a57aca3de9554a20e3e0bfc62885d999b9b410
TheresaDesign <theresa.brand@fh-potsdam.de>
2025-01-13 17:47:15 +0100

    fixing the skip button

--- Assets/JavaScript/basic.js
+++ Assets/JavaScript/basic.js
@@ -9,35 +9,6 @@ function togglePopup() {
   
 //menu
 
-const chapters = document.querySelectorAll(".chapter-point");
-const sections = document.querySelectorAll("section");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      const index = Array.from(sections).indexOf(entry.target);
-      const chapter = chapters[index];
-
-      if (entry.isIntersecting) {
-        // Aktivieren des aktuellen Kapitels
-        chapters.forEach((c) => c.classList.remove("active"));
-        chapter.classList.add("active", "visited");
-      } else {
-        // Entfernen von "visited", wenn man zurückscrollt
-        if (entry.boundingClientRect.top > 0) {
-          chapter.classList.remove("visited");
-        }
-        chapter.classList.remove("active");
-      }
-    });
-  },
-  { threshold: 0.5 } // Aktiviert bei 50% Sichtbarkeit des Abschnitts
-);
-
-Interactive-flowchart
-
-// Beobachten aller Abschnitte
-sections.forEach((section) => observer.observe(section));
 
 
 // Javascript für das scrollen des deconstructing teils
